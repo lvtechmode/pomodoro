2ca9ce4f2b58a28644c67face4b46b215ab50eb5
Liberty <lvtechmode@gmail.com>
2021-09-10 08:36:03 -0700

    github deployment

---
+++ build/precache-manifest.20651b351976967eb79409a269e8f31c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c18e3c7b77defb1ed240c9074bf9d1d",
+    "url": "/pomodoro/index.html"
+  },
+  {
+    "revision": "f126b9039cac074d0e82",
+    "url": "/pomodoro/static/css/main.cb9e985a.chunk.css"
+  },
+  {
+    "revision": "8f60e831c49dd590640b",
+    "url": "/pomodoro/static/js/2.0acf020f.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/pomodoro/static/js/2.0acf020f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f126b9039cac074d0e82",
+    "url": "/pomodoro/static/js/main.b1b43f23.chunk.js"
+  },
+  {
+    "revision": "a9aee71dab78a19abc00",
+    "url": "/pomodoro/static/js/runtime-main.50bf0b17.js"
+  }
+]);
